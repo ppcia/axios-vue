44085983287850549a0b19fc6253fef339107865
root <root@localhost.localdomain>
2020-07-11 21:10:35 +0800

    axios get方法

---
+++ src/views/2-2.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    </div>
+</template>
+
+<script>
+/**
+ * axios请求方法：get, post, put, patch, delete
+ * 
+ * get: 获取数据
+ * post: 提交数据
+ * put: 更新数据（所有数据都推送到后端
+ * patch: 更新数据（只提交修改的数据推送到后端
+ * delete: 删除数据
+ */
+import axios from 'axios'
+
+export default {
+  name: 'Axios2-2',
+  components: {
+  },
+  create(){
+    // http://localhost:8080/data.json
+    axios.get('/data.json').then((res)=>{
+      console.log(res)
+    })
+    axios({
+        method: 'get',
+        url: '/data.json'
+    }).then(res=>{
+        console.log(res)
+    })
+  },
+}
+</script>
